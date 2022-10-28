64a244cf3deb2c3fee3b82db66440784d6c807ea
Richard Peckett <richard.peckett@outlook.com>
2022-10-28 12:48:53 +0100

    remove this git ignore

--- src/Etics.Server/Controllers/RequestKeyboardInputController.cs
+++ src/Etics.Server/Controllers/RequestKeyboardInputController.cs
@@ -14,14 +14,9 @@ public class RequestKeyboardInputController : ControllerBase
     }
 
     //[HttpGet(Name = "GetWeatherForecast")]
-    public void Post()
+    public Task<ActionResult> PostKeyboardInput(EliteShipInputCommand eliteShipInputCommand)
     {
-        // return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-        //     {
-        //         Date = DateTime.Now.AddDays(index),
-        //         TemperatureC = Random.Shared.Next(-20, 55),
-        //         Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-        //     })
-        //     .ToArray();
+
+        return null;
     }
 }
